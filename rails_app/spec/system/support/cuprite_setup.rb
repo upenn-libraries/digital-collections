c6feb8a425167355454eb065bb6dc141e5c71cdf
Patrick Perkins <pperkins@upenn.edu>
2025-04-04 16:18:35 -0400

    update debugger console message to point to new debugger endpoint

--- rails_app/spec/system/support/cuprite_setup.rb
+++ rails_app/spec/system/support/cuprite_setup.rb
@@ -63,7 +63,7 @@ module CupriteHelpers
   end
 
   def debug(binding = nil)
-    $stdout.puts '🔎 Open Chrome inspector at http://localhost:3333'
+    $stdout.puts '🔎 Open Chrome inspector at http://localhost:3333/debugger/'
     return binding.break if binding
 
     page.driver.pause
