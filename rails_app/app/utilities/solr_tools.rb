d31b18d02bdb6b775470917a222e4f61131cf6d1
Mike Kanning <mkanning@upenn.edu>
2025-04-07 23:09:52 -0400

    add :around hook to use to clear Solr index when running designated specs.
    add yard documentation and minor improvements to SolrTools.

--- rails_app/app/utilities/solr_tools.rb
+++ rails_app/app/utilities/solr_tools.rb
@@ -4,18 +4,25 @@
 class SolrTools
   class CommandError < StandardError; end
 
+  # @param collection_name [Object]
+  # @return [Boolean]
   def self.collection_exists?(collection_name)
     list = connection.get('/solr/admin/collections', action: 'LIST')
     collections = JSON.parse(list.body)['collections']
     collections.include? collection_name
   end
 
+  # @param configset_name [String]
+  # @return [Boolean]
   def self.configset_exists?(configset_name)
     list = connection.get('/solr/admin/configs', action: 'LIST')
     configsets = JSON.parse(list.body)['configSets']
     configsets.include? configset_name
   end
 
+  # @param collection_name [String]
+  # @param configset_name [String]
+  # @raise [CommandError]
   def self.create_collection(collection_name, configset_name)
     response = connection.get('/solr/admin/collections',
                               action: 'CREATE', name: collection_name,
@@ -23,6 +30,9 @@ class SolrTools
     raise CommandError, "Solr command failed with response: #{response.body}" unless response.success?
   end
 
+  # @param collection_name [String]
+  # @param documents_file_path [String] path where JSONL file is located
+  # @raise [CommandError]
   def self.load_data(collection_name, documents_file_path)
     body = "{ \"add\": [#{File.readlines(documents_file_path).join(',')}] }"
     response = connection.post("/solr/#{collection_name}/update") do |req|
@@ -33,7 +43,9 @@ class SolrTools
     raise CommandError, "Solr command failed with response: #{response.body}" unless response.success?
   end
 
-  def self.clear_collection(collection_name)
+  # @param collection_name [String] collection to clear - defaults to collection defined in Settings.solr_url
+  # @raise [CommandError]
+  def self.clear_collection(collection_name = current_collection_name)
     body = "{'delete': {'query': '*:*'}}"
     response = connection.post("/solr/#{collection_name}/update") do |req|
       req.params = { commit: 'true' }
@@ -44,22 +56,32 @@ class SolrTools
   end
 
   # Returns name of current collection configured in the Solr URL.
+  # @return [String]
   def self.current_collection_name
     uri = URI.parse(Settings.solr_url)
     uri.path.delete_prefix('/solr/')
   end
 
+  # Check if Solr is up by checking cluster state API
+  # @return [Boolean]
   def self.solr_available?
     connection.get('solr/admin/collections', action: 'CLUSTERSTATUS').success?
   rescue StandardError => _e
     false
   end
 
+  # @param collection_name [String] collection to check - defaults to collection defined in Settings.solr_url
+  # @return [Boolean]
+  def self.collection_empty?(collection_name = current_collection_name)
+    resp = connection.get("solr/#{collection_name}/select", params: { q: '*:*' }, rows: 0)
+    JSON.parse(resp.body)['response']['numFound'].zero?
+  end
+
+  # @return [Faraday::Connection]
   def self.connection
     uri = URI.parse(Settings.solr_url) # Parsing out connection details from URL.
 
     Faraday.new("#{uri.scheme}://#{uri.host}:#{uri.port}") do |faraday|
-      faraday.request :authorization, :basic, uri.user, uri.password
       faraday.adapter :net_http
     end
   end
