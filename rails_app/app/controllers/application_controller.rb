1391e5025455ae74fe1fb5c65897afe9a8cb50a8
Mike Kanning <mkanning@upenn.edu>
2025-04-14 11:31:02 -0400

    remove testing cruft

--- rails_app/app/controllers/application_controller.rb
+++ rails_app/app/controllers/application_controller.rb
@@ -4,6 +4,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  def iiif_test; end
 end
--- rails_app/app/javascript/application.js
+++ rails_app/app/javascript/application.js
@@ -1,8 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "@samvera/clover-iiif"
-import { defineCustomElements } from "@penn-libraries/web"
-
-defineCustomElements();
-
