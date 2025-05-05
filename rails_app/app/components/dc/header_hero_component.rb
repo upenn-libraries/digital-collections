122657c1dd7d917045ebf1f39ab41e84e91f0004
Patrick Perkins <pperkins@upenn.edu>
2025-05-05 14:59:57 -0400

    heading as a slot

--- rails_app/app/components/dc/header_hero_component.rb
+++ rails_app/app/components/dc/header_hero_component.rb
@@ -5,7 +5,7 @@
 module DC
   # Override Header Component
   class HeaderHeroComponent < Blacklight::Component
-    attr_accessor :blacklight_config, :user, :image_url, :heading, :subheading
+    attr_accessor :blacklight_config, :user, :image_url
 
     renders_one :search_bar, lambda { |component: DC::SearchNavbarComponent|
       component.new(blacklight_config: blacklight_config)
@@ -15,19 +15,22 @@ module DC
       UserToolsComponent.new(user: user)
     }
 
+    renders_one :heading
+    renders_one :subheading
+
     # @param blacklight_config [Blacklight::Configuration]
     # @param user [User]
-    def initialize(blacklight_config:, user:, image_url:, heading:, subheading:)
+    def initialize(blacklight_config:, user:, image_url:)
       @blacklight_config = blacklight_config
       @user = user
       @image_url = image_url
-      @heading = heading
-      @subheading = subheading
     end
 
     def before_render
       set_slot(:search_bar, nil) unless search_bar
       set_slot(:user_tools, nil) unless user_tools
+      set_slot(:heading, nil) unless heading
+      set_slot(:subheading, nil) unless subheading
     end
   end
 end
