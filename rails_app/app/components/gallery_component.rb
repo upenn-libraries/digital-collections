c2451bda1c19947969ea9eaa90db432d727c9bc1
Carla Galarza <cgalarza@upenn.edu>
2025-06-04 16:49:33 -0400

    Fixing comment.

--- rails_app/app/components/gallery_component.rb
+++ rails_app/app/components/gallery_component.rb
@@ -3,7 +3,7 @@
 # Customizing Blacklight::Gallery::DocumentComponent to only display gallery index_fields.
 class GalleryComponent < Blacklight::Gallery::DocumentComponent
   def before_render
-    # Only displaying fields that have set the gallery flag to true and customizing HTML tags used.
+    # Only displaying fields that have set the gallery flag to true.
     unless metadata
       gallery_fields = presenter.field_presenters.select { |f| f.field_config.gallery }
       set_slot(:metadata, nil, fields: gallery_fields, field_layout: Gallery::MetadataFieldLayoutComponent)
