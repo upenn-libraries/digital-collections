486132fa31369eb0c6f40ee6b8a7b75bbb8af791
Carla Galarza <cgalarza@upenn.edu>
2025-06-04 14:33:58 -0400

    Adjusting thumbnail size.

--- rails_app/app/helpers/application_helper.rb
+++ rails_app/app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     return nil unless thumbnail_uuid
 
     image_tag(
-      "https://iiif-images.library.upenn.edu/iiif/2/#{thumbnail_uuid}%2Faccess/full/!300,300/0/default.jpg"
+      "https://iiif-images.library.upenn.edu/iiif/2/#{thumbnail_uuid}%2Faccess/full/300,/0/default.jpg"
     )
   end
 end
