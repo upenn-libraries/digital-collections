b986b0cd8622db5e611ea80f1fc2ee098bfa3d78
Carla Galarza <cgalarza@upenn.edu>
2025-06-24 09:56:14 -0400

    Using preview endpoint to grab thumbnail for images (WIP).

--- rails_app/app/helpers/application_helper.rb
+++ rails_app/app/helpers/application_helper.rb
@@ -3,11 +3,9 @@
 # Base application helper
 module ApplicationHelper
   def thumbnail(document, _options)
-    thumbnail_uuid = document[:thumbnail_asset_id_ssi]
-    return nil unless thumbnail_uuid
-
+    # TODO: This preview link doesn't work yet.
     image_tag(
-      "https://iiif-images.library.upenn.edu/iiif/2/#{thumbnail_uuid}%2Faccess/full/300,/0/default.jpg"
+      "https://apotheca.library.upenn.edu/v1/items/#{document[:id]}/preview?size=300x300"
     )
   end
 end
