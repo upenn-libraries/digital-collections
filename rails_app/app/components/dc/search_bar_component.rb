8b0f9d4c75fb878774ede47fced215f319fdb8f0
Jon Earley <earleyj@upenn.edu>
2025-06-26 13:40:02 -0400

    layout adjustments

--- rails_app/app/components/dc/search_bar_component.rb
+++ rails_app/app/components/dc/search_bar_component.rb
@@ -2,13 +2,13 @@
 
 # copied from Blacklight 9
 
-require_dependency Blacklight::Engine.root.join('app', 'components', 'blacklight', 'search_bar_component').to_s
-
 module DC
   # Override Search Bar Component
   class SearchBarComponent < Blacklight::SearchBarComponent
     def initialize(**)
       super
+
+      @classes = %w[dc-search-box]
     end
   end
 end
