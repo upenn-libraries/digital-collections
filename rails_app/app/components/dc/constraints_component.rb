7561da16f6ee49bcc8b6eebc746e9c25566aa9fa
Patrick Perkins <pperkins@upenn.edu>
2025-07-14 14:14:19 -0400

    add override comments

--- rails_app/app/components/dc/constraints_component.rb
+++ rails_app/app/components/dc/constraints_component.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Override Component from Blacklight 9.0.0.beta1 to remove "visually-hidden" on constraints headers,
+# some additional styling
 module DC
   # Override constraints
   class ConstraintsComponent < Blacklight::ConstraintsComponent
