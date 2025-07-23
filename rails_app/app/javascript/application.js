d7a416584880a79d5e3a9c388314852d45520663
Patrick Perkins <pperkins@upenn.edu>
2025-07-23 10:59:50 -0400

    pull in pennlibs design system with CDN script tag

--- rails_app/app/javascript/application.js
+++ rails_app/app/javascript/application.js
@@ -2,7 +2,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap";
 import Blacklight from "blacklight-frontend";
-import "@penn-libraries/web/dist/web/web.esm.js"
 
 import BlacklightRangeLimit from "blacklight-range-limit";
 BlacklightRangeLimit.init({ onLoadHandler: Blacklight.onLoad });
