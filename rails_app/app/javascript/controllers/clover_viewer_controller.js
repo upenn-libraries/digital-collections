8cd1ff36284ac3238d78dce717fc53a878e0c411
Patrick Perkins <pperkins@upenn.edu>
2025-08-01 12:10:32 -0400

    format js with prettier

--- rails_app/app/javascript/controllers/clover_viewer_controller.js
+++ rails_app/app/javascript/controllers/clover_viewer_controller.js
@@ -4,26 +4,26 @@ import Viewer from "@samvera/clover-iiif/viewer";
 import { Controller } from "@hotwired/stimulus";
 
 const options = {
-	showTitle: false,
-	informationPanel: {
-		open: false,
-	},
+  showTitle: false,
+  informationPanel: {
+    open: false,
+  },
 };
 
 export default class CloverViewerController extends Controller {
-	static values = {
-		url: String,
-	};
+  static values = {
+    url: String,
+  };
 
-	connect() {
-		const root = createRoot(this.element);
-		root.render(this.getViewer(this.urlValue));
-	}
+  connect() {
+    const root = createRoot(this.element);
+    root.render(this.getViewer(this.urlValue));
+  }
 
-	getViewer(url) {
-		return createElement(Viewer, {
-			iiifContent: url,
-			options: options,
-		});
-	}
+  getViewer(url) {
+    return createElement(Viewer, {
+      iiifContent: url,
+      options: options,
+    });
+  }
 }
