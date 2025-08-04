46247ae9609d5fb059f7600bc4a9ce3fb4798694
Amrey Mathurin <mamrey@upenn.edu>
2025-08-04 15:54:00 -0400

    fix issue with merge

--- rails_app/app/components/dc/show_document_component.rb
+++ rails_app/app/components/dc/show_document_component.rb
@@ -2,7 +2,8 @@
 
 module DC
   # Override DocumentComponent from Blacklight 9.0.0beta1 to add Clover IIIF Viewer
-  class ShowDocumentComponent < Blacklight::DocumentComponent; end
-  # file download component listing download links for all non IIIF assets
-  renders_one :file_download, ->(assets: @document.non_iiif_assets) { DC::FileDownloadComponent.new(assets: assets) }
+  class ShowDocumentComponent < Blacklight::DocumentComponent
+    # file download component listing download links for all non IIIF assets
+    renders_one :file_download, ->(assets: @document.non_iiif_assets) { DC::FileDownloadComponent.new(assets: assets) }
+  end
 end
