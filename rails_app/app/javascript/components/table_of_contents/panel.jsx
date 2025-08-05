03a618cf0e64eea6de9e4a314f6f7338fe5a6cec
Patrick Perkins <pperkins@upenn.edu>
2025-08-05 17:01:31 -0400

    fix indentation

--- rails_app/app/javascript/components/table_of_contents/panel.jsx
+++ rails_app/app/javascript/components/table_of_contents/panel.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 
 export default function Panel({ useViewerDispatch, useViewerState }) {
-	const viewerState = useViewerState();
-	const dispatch = useViewerDispatch();
+  const viewerState = useViewerState();
+  const dispatch = useViewerDispatch();
 
-	const { activeManifest, vault } = viewerState;
-	const manifestData = vault.toPresentation3({
-		id: activeManifest,
-		type: "Manifest",
-	});
+  const { activeManifest, vault } = viewerState;
+  const manifestData = vault.toPresentation3({
+    id: activeManifest,
+    type: "Manifest",
+  });
 
-	const containerStyle = { padding: "0px 1.618rem 2rem" };
-	const listStyleClasses = ["list-unstyled", "list-group"];
-	const entryStyleClasses = ["list-group-item", "list-group-item-action"];
+  const containerStyle = { padding: "0px 1.618rem 2rem" };
+  const listStyleClasses = ["list-unstyled", "list-group"];
+  const entryStyleClasses = ["list-group-item", "list-group-item-action"];
 
-	const handleClick = (targetCanvas) => {
-		dispatch({
-			type: "updateActiveCanvas",
-			canvasId: targetCanvas.id,
-		});
-	};
+  const handleClick = (targetCanvas) => {
+    dispatch({
+      type: "updateActiveCanvas",
+      canvasId: targetCanvas.id,
+    });
+  };
 
-	return (
-		<div style={containerStyle}>
-			<ul className={listStyleClasses.join(" ")}>
-				{manifestData.structures.map((range) => (
-					<li key={range.id}>
-						<button
-							type="button"
-							className={entryStyleClasses.join(" ")}
-							onClick={() => handleClick(range.items[0])}
-						>
-							{range.label.none[0]}
-						</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div style={containerStyle}>
+      <ul className={listStyleClasses.join(" ")}>
+        {manifestData.structures.map((range) => (
+          <li key={range.id}>
+            <button
+              type="button"
+              className={entryStyleClasses.join(" ")}
+              onClick={() => handleClick(range.items[0])}
+            >
+              {range.label.none[0]}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
