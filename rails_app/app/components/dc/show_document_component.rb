864f54d170f2d8896431761a861d97c882b8ea14
Amrey Mathurin <mamrey@upenn.edu>
2025-08-08 15:54:18 -0400

    Set default download and share slot in ShowDocumentComponent

--- rails_app/app/components/dc/show_document_component.rb
+++ rails_app/app/components/dc/show_document_component.rb
@@ -6,5 +6,10 @@ module DC
   #  - render DownloadAndShare component
   class ShowDocumentComponent < Blacklight::DocumentComponent
     renders_one :download_and_share, DC::DownloadAndShareComponent
+
+    def before_render
+      super
+      with_download_and_share(document: @document) unless download_and_share
+    end
   end
 end
