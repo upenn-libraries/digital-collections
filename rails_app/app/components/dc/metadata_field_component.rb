293091bc6c08f53da63135f525b58a7984716e12
Amrey Mathurin <mamrey@upenn.edu>
2025-08-13 08:01:56 -0400

    Remove field rendering from metadata field component

--- rails_app/app/components/dc/metadata_field_component.rb
+++ rails_app/app/components/dc/metadata_field_component.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
 module DC
-  # Copied from Blacklight 9.0.0beta1@de55db to render each element in a multivalued field with a single dd
+  # Copied from Blacklight 9.0.0beta1@de55db to render our custom MetadataFieldLayoutComponent
   class MetadataFieldComponent < Blacklight::MetadataFieldComponent
     def initialize(field:, layout: nil, show: nil, view_type: nil)
       super
       @layout = DC::MetadataFieldLayoutComponent
-    end
-
-    # @return [Array]
-    def render_field
-      operations = Blacklight::Rendering::Pipeline.operations - [Blacklight::Rendering::Join]
-      Blacklight::Rendering::Pipeline.new(@field.values, @field.field_config, @field.document, @field.view_context,
-                                          operations, @field.options).render
     end
   end
 end
