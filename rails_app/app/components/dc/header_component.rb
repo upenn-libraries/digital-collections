ec4138ff7badcbe332da72f75f04a6608034351b
Carla Galarza <cgalarza@upenn.edu>
2025-08-13 15:15:07 -0400

    Removing log-in button

--- rails_app/app/components/dc/header_component.rb
+++ rails_app/app/components/dc/header_component.rb
@@ -1,33 +1,26 @@
 # frozen_string_literal: true
 
 module DC
-  # This is a custom header component that render a search bar and user tools.
+  # This is a custom header component that render a search bar.
   # It's used in two places:
   # 1. We set the header to this component in the catalog controller to render on all Blacklight pages.
   # 2. We tell the base Blacklight layout to render the HeaderHeroComponent on static pages,
   #    which has a slot for this component.
   class HeaderComponent < Blacklight::Component
-    attr_accessor :blacklight_config, :user, :theme
+    attr_accessor :blacklight_config, :theme
 
     renders_one :search_bar, lambda { |component: DC::SearchNavbarComponent|
       component.new(blacklight_config: blacklight_config)
     }
 
-    renders_one :user_tools, lambda {
-      UserToolsComponent.new(user: user)
-    }
-
     # @param blacklight_config [Blacklight::Configuration]
-    # @param user [User]
-    def initialize(blacklight_config:, user:, theme: :light)
+    def initialize(blacklight_config:, theme: :light)
       @blacklight_config = blacklight_config
-      @user = user
       @theme = theme
     end
 
     def before_render
       set_slot(:search_bar, nil) unless search_bar
-      set_slot(:user_tools, nil) unless user_tools
     end
   end
 end
--- rails_app/app/components/dc/header_hero_component.rb
+++ rails_app/app/components/dc/header_hero_component.rb
@@ -4,20 +4,18 @@ module DC
   # Custom hero component that renders a header, a picture, and a heading.
   # Used by the base Blacklight layout to render the hero on static pages.
   class HeaderHeroComponent < Blacklight::Component
-    attr_accessor :blacklight_config, :user
+    attr_accessor :blacklight_config
 
     renders_one :header, lambda {
-      HeaderComponent.new(blacklight_config: blacklight_config, user: user, theme: :dark)
+      HeaderComponent.new(blacklight_config: blacklight_config, theme: :dark)
     }
     renders_one :picture
     renders_one :heading
     renders_one :subheading
 
     # @param blacklight_config [Blacklight::Configuration]
-    # @param user [User]
-    def initialize(blacklight_config:, user:)
+    def initialize(blacklight_config:)
       @blacklight_config = blacklight_config
-      @user = user
     end
 
     def before_render
