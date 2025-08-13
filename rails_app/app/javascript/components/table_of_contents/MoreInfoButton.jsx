dc80c0c6fdf5e456fcbb8eb096a0c54ce681d170
Patrick Perkins <pperkins@upenn.edu>
2025-08-13 15:50:04 -0400

    remove unused useState from more info button

--- rails_app/app/javascript/components/table_of_contents/MoreInfoButton.jsx
+++ rails_app/app/javascript/components/table_of_contents/MoreInfoButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function MoreInfoButton({ useViewerState, useViewerDispatch }) {
   const viewerState = useViewerState();
