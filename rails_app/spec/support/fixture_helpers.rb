a35557093c352b96c7d3f0383ad985c0a60a1ed1
Amrey Mathurin <mamrey@upenn.edu>
2025-08-22 14:32:14 -0400

    Port over pperkin's fixture helper from find

---
+++ rails_app/spec/support/fixture_helpers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module FixtureHelpers
+  # @param filename [String]
+  # @param directory [Nil, String]
+  # @return [String]
+  def json_fixture(filename, directory = nil)
+    filename = "#{filename}.json" unless filename.ends_with?('.json')
+    dirs = ['json', directory.to_s, filename].compact_blank
+    File.read(File.join(fixture_paths, dirs))
+  end
+end
