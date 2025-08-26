334b2c7ba7c9d5ad2a2b22e2ef13b716efca9518
Patrick Perkins <pperkins@upenn.edu>
2025-08-26 12:19:23 -0400

    move svg icons into their own components

--- rails_app/app/javascript/components/media_download/DownloadButton.jsx
+++ rails_app/app/javascript/components/media_download/DownloadButton.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import * as Popover from "@radix-ui/react-popover";
 import { makeBlob, mimicDownload } from "@samvera/image-downloader";
+import * as Popover from "@radix-ui/react-popover";
+import Download from "../viewer_icons/Download";
 
 export default function DownloadButton({ useViewerState }) {
   const viewerState = useViewerState();
@@ -50,18 +51,7 @@ export default function DownloadButton({ useViewerState }) {
           className="dc-iiif-viewer-controls__button"
           aria-label="Download image"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-download"
-            viewBox="0 0 16 16"
-            aria-hidden="true"
-          >
-            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
-          </svg>
+          <Download />
         </button>
       </Popover.Trigger>
 
