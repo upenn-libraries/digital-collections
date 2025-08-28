f94e295c45886d37dee8dd31e0c177c036562c9f
Patrick Perkins <pperkins@upenn.edu>
2025-08-28 11:36:54 -0400

    re-inline download SVG;

--- rails_app/app/javascript/components/media_download/DownloadButton.jsx
+++ rails_app/app/javascript/components/media_download/DownloadButton.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeBlob, mimicDownload } from "@samvera/image-downloader";
 import * as Popover from "@radix-ui/react-popover";
-import Download from "@components/viewer_icons/Download";
 
 const DOWNLOAD_OPTIONS = {
   small: {
@@ -63,7 +62,16 @@ export default function DownloadButton({ useViewerState }) {
           className="dc-iiif-viewer-controls__button"
           aria-label="Download image"
         >
-          <Download />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+          </svg>
         </button>
       </Popover.Trigger>
 
