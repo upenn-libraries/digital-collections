54ef3c750add9f11a404a52b6bd31d5301a1bf8e
Patrick Perkins <pperkins@upenn.edu>
2025-08-28 11:39:43 -0400

    un-inline svgs

--- rails_app/app/javascript/components/table_of_contents/MoreInfoButton.jsx
+++ rails_app/app/javascript/components/table_of_contents/MoreInfoButton.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ClosePanel from "@components/viewer_icons/ClosePanel";
-import OpenPanel from "@components/viewer_icons/OpenPanel";
 
 export default function MoreInfoButton({ useViewerState, useViewerDispatch }) {
   const viewerState = useViewerState();
@@ -22,7 +20,29 @@ export default function MoreInfoButton({ useViewerState, useViewerDispatch }) {
       aria-label="More info"
       aria-expanded={viewerState.isInformationOpen}
     >
-      {viewerState.isInformationOpen ? <ClosePanel /> : <OpenPanel />}
+      {viewerState.isInformationOpen ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path d="M300-640v320l160-160-160-160ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm440-80h120v-560H640v560Zm-80 0v-560H200v560h360Zm80 0h120-120Z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path d="M460-320v-320L300-480l160 160ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm440-80h120v-560H640v560Zm-80 0v-560H200v560h360Zm80 0h120-120Z" />
+        </svg>
+      )}
     </button>
   );
 }
