4bbb789e13ee1c06b19a47689649ac835ec67879
Amrey Mathurin <mamrey@upenn.edu>
2026-04-17 12:56:26 -0400

    Implement viewer skeleton loader

---
+++ rails_app/app/javascript/components/viewer_skeleton/LoadingComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+export default function LoadingComponent(){
+    return(
+        <>
+            <div className="d-flex flex-column mb-3">
+                <div className="align-self-center placeholder placeholder-wave"  style={{width: "250px", height: "400px"}}/>
+                <div className="d-flex flex-row justify-content-center" style={{padding: "1.618rem 0px", gap: "0.4rem"}}>
+                    <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
+                    <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
+                    <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
+                    <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
+                    <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
+                    <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ rails_app/app/javascript/components/viewer_skeleton/ViewerSkeleton.jsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+
+// Placeholder component to render while fetching manifest
+import LoadingComponent from "@components/viewer_skeleton/LoadingComponent";
+
+// Clover-IIIF Viewer
+import Viewer from "@samvera/clover-iiif/viewer";
+// Table of Contents components
+import EntriesPanel from "@components/table_of_contents/EntriesPanel";
+import MoreInfoButton from "@components/table_of_contents/MoreInfoButton";
+
+// Viewer asset download component
+import DownloadButton from "@components/media_download/DownloadButton";
+
+// Viewer configuration
+
+const contentsPlugin = {
+    id: "toc-list",
+    imageViewer: {
+        controls: {
+            component: MoreInfoButton,
+        },
+    },
+    informationPanel: {
+        component: EntriesPanel,
+        label: { none: ["Table of contents"] },
+    },
+};
+
+const downloadPlugin = {
+    id: "download-button",
+    imageViewer: {
+        controls: {
+            component: DownloadButton,
+        },
+    },
+};
+
+const customTheme = {
+    colors: {
+        /**
+         * Black and dark grays in a light theme.
+         * All must contrast to 4.5 or greater with `secondary`.
+         */
+        primary: "#2D3545",
+        primaryMuted: "#595F6A",
+        primaryAlt: "#2D3545",
+
+        /**
+         * Key brand color(s).
+         * `accent` must contrast to 4.5 or greater with `secondary`.
+         */
+        accent: "#0E5696",
+        accentMuted: "#0E5696",
+        accentAlt: "#0E5696",
+
+        /**
+         * White and light grays in a light theme.
+         * All must contrast to 4.5 or greater with `primary` and  `accent`.
+         */
+        secondary: "#FFFFFF",
+        secondaryMuted: "#F5F5F6",
+        secondaryAlt: "#F5F5F6",
+    },
+    fonts: {
+        sans: "proxima-nova, system-ui, sans-serif",
+        display: "proxima-nova, system-ui, sans-serif",
+    },
+};
+
+const options = {
+    showTitle: false,
+    showDownload: false,
+    showIIIFBadge: false,
+    informationPanel: {
+        open: false,
+        renderToggle: false,
+    },
+};
+
+export default function ViewerSkeleton({ manifestUrl }) {
+    const [manifest, setManifest] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(manifestUrl);
+            const json = await response.json();
+            setManifest(json);
+        })();
+    }, [manifestUrl]);
+
+    function plugins() {
+        return manifest.structures && manifest.structures.length > 0
+            ? [downloadPlugin, contentsPlugin]
+            : [downloadPlugin];
+    }
+
+    if (!manifest) {
+        return <LoadingComponent />;
+    } else {
+        return (
+            <Viewer
+                iiifContent={manifest}
+                options={options}
+                plugins={plugins()}
+                customTheme={customTheme}
+            />
+        );
+    }
+}
