ede5a5007044860a538d8083b7019061bd696f4d
Amrey Mathurin <mamrey@upenn.edu>
2026-04-20 13:53:49 -0400

    remove fragment

--- rails_app/app/javascript/components/viewer_skeleton/LoadingComponent.jsx
+++ rails_app/app/javascript/components/viewer_skeleton/LoadingComponent.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 export default function LoadingComponent(){
     return(
-        <>
             <div className="d-flex flex-column mb-3">
                 <div className="align-self-center placeholder placeholder-wave"  style={{width: "250px", height: "400px"}}/>
                 <div className="d-flex flex-row justify-content-center" style={{padding: "1.618rem 0px", gap: "0.4rem"}}>
@@ -13,6 +12,5 @@ export default function LoadingComponent(){
                     <div className="placeholder placeholder-wave" style={{height: "100px", width: "161.8px"}}/>
                 </div>
             </div>
-        </>
     )
 }
