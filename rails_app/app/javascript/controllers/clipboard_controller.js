82303ced0671265933fba35dd88b79f3f310e0be
Carla Galarza <cgalarza@upenn.edu>
2026-05-07 09:51:34 -0400

    Adding tooltip to let user know that the link had been copied

--- rails_app/app/javascript/controllers/clipboard_controller.js
+++ rails_app/app/javascript/controllers/clipboard_controller.js
@@ -1,10 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
+import { Tooltip } from "bootstrap";
 
-// Controller to copy text from link into clipboard.
+// Controller to copy text from source into clipboard.
 export default class extends Controller {
-    static targets = [ "source" ]
+    static targets = [ "source", "button" ]
+
+    connect() {
+        this.tooltip = new Tooltip(this.buttonTarget, { title: 'Copied!', trigger: 'manual' });
+    }
+
+    disconnect() {
+        this.tooltip?.dispose();
+    }
 
     copy() {
-        navigator.clipboard.writeText(this.sourceTarget.text)
+        // Copy text.
+        navigator.clipboard.writeText(this.sourceTarget.text);
+
+        // Show tooltip to let users know value has been copied.
+        this.tooltip.show();
+
+        // Hide tooltip after a few seconds.
+        setTimeout(() => {
+            this.tooltip.hide();
+        }, 3000);
     }
 }
