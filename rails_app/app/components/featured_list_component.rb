3c823273bb81d7e98afc4cfd753c03fc0e97a4ab
Carla Galarza <cgalarza@upenn.edu>
2026-05-28 09:11:19 -0400

    Adding documentation for FeaturedListComponent

--- rails_app/app/components/featured_list_component.rb
+++ rails_app/app/components/featured_list_component.rb
@@ -4,6 +4,10 @@
 class FeaturedListComponent < ViewComponent::Base
   renders_many :facets, ->(**params) { FeaturedFacetComponent.new(**params, heading_tag: @heading_tag, facet: @facet) }
 
+  # List of featured facets.
+  #
+  # @param facet [String] facet; solr field
+  # @param heading_tag [Symbol] html tag for list item headers
   def initialize(facet: nil, heading_tag: :h2)
     @heading_tag = heading_tag
     @facet = facet
